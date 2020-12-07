ea74480fc779ba5d8d267587acfdb5a4177dd398
Ali Shah <s.ashah@outlook.com>
2020-12-07 08:36:32 -0500

    [Finished] Ready to submit

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,37 +2,41 @@ import { useState } from "react";
 import './Calculator.css';
 
 function Calculator() {
-  const [name, setName] = useState("");
+  const [billInput, setBillInput] = useState(10.00.toFixed(2));
+  const [tipInput, setTipInput] = useState(15);
+  const [peopleInput, setPeopleInput] = useState(2);
+
+  const billValue = parseFloat(billInput);
+  const tipValue = parseFloat(tipInput);
+  const numPeople = parseInt(peopleInput);
+
+  const billDisplay = billValue * (1 + tipValue/100) / numPeople;
+  const tipDisplay = billValue * (tipValue/100) / numPeople;
 
   return (
     <main className="Calculator">
       <h1>Tip Calculator</h1>
-
       <p>Total Per Person:</p>
-      <div className="display">0.00</div>
-
+      <div className="display"><strong>{billDisplay.toFixed(2)}</strong></div>
       <p>Tip Per Person:</p>
-      <div className="display">0.00</div>
-
+      <div className="display">{tipDisplay.toFixed(2)}</div>
       <p>Bill:</p>
       <input
         type="text"
-        value={name}
-        onChange={(e) => {setName(e.target.value)}}
+        value={billInput}
+        onChange={(e) => {setBillInput(e.target.value)}}
       />
-
       <p>Tip Percentage:</p>
       <input
         type="text"
-        value={name}
-        onChange={(e) => {setName(e.target.value)}}
+        value={tipInput}
+        onChange={(e) => {setTipInput(e.target.value)}}
       />
-
       <p>Number of People:</p>
       <input
         type="text"
-        value={name}
-        onChange={(e) => {setName(e.target.value)}}
+        value={peopleInput}
+        onChange={(e) => {setPeopleInput(e.target.value)}}
       />
     </main>
   );
